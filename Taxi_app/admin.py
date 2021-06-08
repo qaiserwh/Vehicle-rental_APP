14e5b02f83127c8bf63a1979e2108c0f7de71e0c
qaiserwh <78928887+qaiserwh@users.noreply.github.com>
2021-06-08 17:41:48 +0300

    Commit

--- Taxi_app/admin.py
+++ Taxi_app/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
-from .models import*
+from.models import*
 # Register your models here.
 class TaxiAdmin(admin.ModelAdmin):
-    list_display=['The_beneficial_driver','status','client_name','phonenumber','age_of_driver','location','To']
-admin.site.site_header ='Site Administration'
-admin.site.site_title ='Site Administration'
-admin.site.register(Drivers)
+    list_display=['The_beneficial_driver','status','client_name','phonenumber','time','location','To']
+    search_fields=['The_beneficial_driver']
+class BusAdmin(admin.ModelAdmin):
+    list_display=['The_beneficial_driver','status','client_name','phonenumber','time','location','To']
+    search_fields=['The_beneficial_driver']
+class DriverAdmin(admin.ModelAdmin):
+    list_display=['name','email','phonenumber','Type_of_car','carnumber']
+class TruckseAdmin(admin.ModelAdmin):
+    list_display=['The_beneficial_driver','status','client_name','phonenumber','time','Type_of_load','location','To']
+class VipBusAdmin(admin.ModelAdmin):
+    list_display=['The_beneficial_driver','status','clientname','phonenumber','price','time','location','To']
+    search_fields=['The_beneficial_driver']
+
+
+
+admin.site.site_header ='الادارة'
+admin.site.site_title ='صفحة الادارة'
+admin.site.register(Drivers,DriverAdmin)
 admin.site.register(Taxie ,TaxiAdmin)
-admin.site.register(Buse)
-admin.site.register(Truckse)
+admin.site.register(Buse,BusAdmin)
+admin.site.register(VipBuse,VipBusAdmin)
+admin.site.register(Truckse,TruckseAdmin)
