062752e7bdcf3253e763c85330ae23b6a5a5aef3
qaiserwh <78928887+qaiserwh@users.noreply.github.com>
2021-06-08 17:40:56 +0300

    Commit

--- Taxi_app/forms.py
+++ Taxi_app/forms.py
@@ -2,7 +2,7 @@
 
 from Taxi_app.models import Taxie,Buse,Truckse,Drivers
 from django import forms
-from . models import Buse, Taxie
+from . models import Buse, Taxie, VipBuse
 
 
 
@@ -18,7 +18,7 @@ class TaxiForm(forms.ModelForm):
         fields =[
             'client_name',
             'phonenumber',
-            'age_of_driver',
+            'time',
             'location',
             'To',
         ] 
@@ -27,7 +27,7 @@ class TaxiForm(forms.ModelForm):
         widgets ={
             'client_name':forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Name'}),
             'phonenumber': forms.NumberInput(attrs={'class':'form-control', 'placeholder':'Mobile No +964  '}),
-            'age_of_driver': forms.NumberInput(attrs={'class':'form-control', 'placeholder':'----'}),
+            'time':forms.Select(attrs={'class':'form-control'}),
             'location': forms.TextInput(attrs={'class':'form-control', 'placeholder':' location'}),
             'To': forms.TextInput(attrs={'class':'form-control', 'placeholder':'To'}),
         }
@@ -38,6 +38,7 @@ class BusForm(forms.ModelForm):
         fields =[
             'client_name',
             'phonenumber',
+            'time',
             'location',
             'To',
         ] 
@@ -46,6 +47,7 @@ class BusForm(forms.ModelForm):
         widgets ={
             'client_name': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Name'}),
             'phonenumber': forms.NumberInput(attrs={'class':'form-control', 'placeholder':'Mobile No +964  '}),
+            'time':forms.Select(attrs={'class':'form-control'}),
             'location': forms.TextInput(attrs={'class':'form-control', 'placeholder':' location'}),
             'To': forms.TextInput(attrs={'class':'form-control', 'placeholder':'To'}),
         }
@@ -58,6 +60,7 @@ class TrucksForm(forms.ModelForm):
             'client_name',
             'phonenumber',
             'Type_of_load',
+            'time',
             'location',
             'To',
         ] 
@@ -67,6 +70,7 @@ class TrucksForm(forms.ModelForm):
             'client_name': forms.TextInput(attrs={'class':'form-control','placeholder':'Name'}),
             'phonenumber': forms.NumberInput(attrs={'class':'form-control', 'placeholder':'Mobile No +964  '}),
             'Type_of_load': forms.TextInput(attrs={'class':'form-control','placeholder':''}),
+            'time':forms.Select(attrs={'class':'form-control'}),
             'location': forms.TextInput(attrs={'class':'form-control','placeholder':' location'}),
             'To': forms.TextInput(attrs={'class':'form-control','placeholder':'To'}),
         }
@@ -77,7 +81,6 @@ class DriverForm(forms.ModelForm):
         model =Drivers
         fields =[
             'name',
-            'age',
             'email',
             'phonenumber',
             'Type_of_car',
@@ -87,9 +90,33 @@ class DriverForm(forms.ModelForm):
 
         widgets ={
             'name': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Name'}),
-            'age': forms.NumberInput(attrs={'class':'form-control', 'placeholder':'age'}),
             'email': forms.TextInput(attrs={'class':'form-control', 'placeholder':' email'}),
             'phonenumber': forms.NumberInput(attrs={'class':'form-control', 'placeholder':'Mobile No +964  '}),
             'Type_of_car': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Name of car'}),
             'carnumber': forms.TextInput(attrs={'class':'form-control', 'placeholder':'----'}),
         }
+        
+
+
+class VipBusForm(forms.ModelForm):
+    class Meta:
+        model =VipBuse
+        fields =[
+            'clientname',
+            'phonenumber',
+            'price',
+            'time',
+            'location',
+            'To',
+        ] 
+        
+
+        widgets ={
+            'clientname': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Name'}),
+            'phonenumber': forms.NumberInput(attrs={'class':'form-control', 'placeholder':'Mobile No +964  '}),
+            'price':forms.Select(attrs={'class':'form-control'}),
+            'time':forms.Select(attrs={'class':'form-control'}),
+            'location': forms.TextInput(attrs={'class':'form-control', 'placeholder':' location'}),
+            'To': forms.TextInput(attrs={'class':'form-control', 'placeholder':'To'}),
+        }
+
